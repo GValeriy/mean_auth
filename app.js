9543b2f2e83aa1361547d2afbf4bec7f8a74f98d
Valery <goncharov.valery@mail.ru>
2017-03-13 17:43:55 +0200

    Initiate

---
+++ app.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+app.use(express.static(__dirname + '/client'));
+app.use(bodyParser.json());
+
+Worker = require('./models/worker');
+
+//connect to mongoose
+mongoose.connect('mongodb://localhost/workerstore');
+var db = mongoose.connection;
+
+app.get('/', function (req,res) {
+    res.send('Hello word');
+});
+
+
+app.get('/api/workers',function (req,res) {
+    Worker.getWorkers(function (err, workers) {
+        if(err){
+            throw err;
+        }
+        res.json(workers);
+    })
+});
+
+app.get('/api/workers/:_id',function (req,res) {
+    Worker.getWorkerById(req.params._id, function (err, worker) {
+        if(err){
+            throw err;
+        }
+        res.json(worker);
+    })
+});
+
+app.post('/api/workers',function (req,res) {
+    var worker = req.body;
+    Worker.addWorker(worker, function (err, worker) {
+        if(err){
+            throw err;
+        }
+        res.json(worker);
+    })
+});
+
+app.put('/api/workers/:_id',function (req,res) {
+    var id = req.params._id;
+    var worker = req.body;
+    Worker.updateWorker(id, worker, {}, function (err, worker) {
+        if(err){
+            throw err;
+        }
+        res.json(worker);
+    })
+});
+
+app.delete('/api/workers/:_id',function (req,res) {
+    var id = req.params._id;
+    Worker.removeWorker(id, function (err, worker) {
+        if(err){
+            throw err;
+        }
+        res.json(worker);
+    })
+});
+
+app.listen(3000);
+console.log('Running on port 3000');
---
+++ models/worker.js
@@ -0,0 +1,76 @@
+var mongoose = require('mongoose');
+
+//Workers Schema
+
+var workerSchema = mongoose.Schema({
+    name:{
+        type: String,
+        // required:true
+    },
+    patronymic:{
+        type: String
+    },
+    post:{
+        type: String,
+        // required:true
+    },
+    surname:{
+        type: String,
+        // required:true
+    },
+    sex:{
+        type: String
+    },
+    phone:{
+        type: String
+    },
+    work_start:{
+        type: String
+    },
+    work_stop:{
+        type: String
+    },
+    create_date:{
+        type: Date,
+        default: Date.now
+    }
+});
+var Worker = module.export = mongoose.model('Worker', workerSchema);
+
+//GET workers
+module.exports.getWorkers = function(callback, limit) {
+    Worker.find(callback).limit(limit);
+};
+
+//Get worker
+module.exports.getWorkerById = function(id, callback) {
+    Worker.findById(id, callback);
+};
+
+//ADD worker
+module.exports.addWorker = function(worker, callback) {
+    Worker.create(worker, callback);
+};
+
+//Update Worker
+module.exports.updateWorker = function (id, worker, options, callback) {
+    var query = {_id: id};
+    var update = {
+        name:worker.name,
+        patronymic:worker.patronymic,
+        post:worker.post,
+        surname:worker.surname,
+        sex:worker.sex,
+        phone:worker.phone,
+        work_start:worker.work_start,
+        work_stop:worker.work_stop
+    };
+    Worker.findOneAndUpdate(query, update, options, callback);
+};
+
+
+//Delete Worker
+module.exports.removeWorker = function(id,callback) {
+    var query = {_id: id};
+    Worker.remove(query, callback);
+};
